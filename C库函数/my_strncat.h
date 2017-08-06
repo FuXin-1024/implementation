48e0ca21064c473fdb24fa9c2534dd97e371bae3
FuXin-1024 <1007160080@qq.com>
2017-08-06 16:18:02 +0800

    妯℃嫙瀹炵幇strncat

---
+++ C库函数/my_strncat.h
@@ -0,0 +1,30 @@
+#pragma warning(disable:4996)
+#include<iostream>
+#include<assert.h>
+using namespace std;
+
+//模拟实现strncat函数
+//函数原型:char* strncat(char* dest,const char* src,size_t count)
+
+char* my_strncat(char* dest, const char* src, size_t count)
+{
+	assert(dest);
+	assert(src);
+
+	char* ret = dest;
+	while (*dest)
+		dest++;
+	while (count--)
+	{
+		*dest++ = *src++;
+	}
+
+	return ret;
+}
+
+void my_strcatTest()
+{
+	char dest[50] = { "qwe" };
+	my_strncat(dest, "12",10);
+	my_strncat(dest, "asd",2);
+}
